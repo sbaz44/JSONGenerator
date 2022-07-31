64167b978280d87bfe237f89576eeb92eb52fcae
Shahbaz Shaikh <shahbaz.shaikh@devomark.com>
2022-08-01 01:52:06 +0530

    Updated edit service

--- src/pages/Canvas.js
+++ src/pages/Canvas.js
@@ -2,25 +2,15 @@ import React, { useEffect, useState } from "react";
 let LOIClicks = 0;
 let lastClick = [0, 0];
 let dots = 0;
-let finalROI = [];
-
-//  setshowErrorModal((prevState) => ({
-//             ...prevState,
-//             showPop: true,
-//             msg: "OTP limit exhausted, Please try again later!",
-//             type: "alert",
-//             header: "Error",
-//           }));
+let finalROI = []; //only to pass it as param in INSIDE function
 export default function Canvas() {
   const [Demo, setDemo] = useState(false);
   const [Rows, setRows] = useState(0);
   const [Type, setType] = useState("ROI");
   const [Columns, setColumns] = useState(0);
   const [ROICord, setROICord] = useState([]);
+  const [LOICord, setLOICord] = useState([]);
   const inside = (point, vs) => {
-    // ray-casting algorithm based on
-    // https://wrf.ecse.rpi.edu/Research/Short_Notes/pnpoly.html/pnpoly.html
-
     let x = point[0],
       y = point[1];
 
@@ -116,10 +106,44 @@ export default function Canvas() {
     ctx.lineTo(lineto[0], lineto[1]);
     ctx.stroke();
   };
+  const redrawCanvas = (roiData = ROICord, loiData = LOICord) => {
+    let ctx = document.getElementById("canvas").getContext("2d");
+    ctx.clearRect(0, 0, 640, 480);
+    // ROICord.map((item, idx) =>
+    //   idx === 3
+    //     ? drawROI(
+    //         [ROICord[idx].x, ROICord[idx].y],
+    //         [ROICord[0].x, ROICord[0].y]
+    //       )
+    //     : drawROI(
+    //         [ROICord[idx].x, ROICord[idx].y],
+    //         [ROICord[idx + 1].x, ROICord[idx + 1].y]
+    //       )
+    // );
+    roiData.map((item, idx) =>
+      idx === 3
+        ? drawROI(
+            [roiData[idx].x, roiData[idx].y],
+            [roiData[0].x, roiData[0].y]
+          )
+        : drawROI(
+            [roiData[idx].x, roiData[idx].y],
+            [roiData[idx + 1].x, roiData[idx + 1].y]
+          )
+    );
+
+    loiData.map((item, idx) => {
+      ctx.beginPath();
+      ctx.moveTo(item.x1, item.y1);
+      ctx.lineTo(item.x2, item.y2, 6);
+      ctx.strokeStyle = "#ff2626";
+      ctx.stroke();
+    });
+    setROICord(roiData);
+    setLOICord(loiData);
+  };
   const handleROI = (e) => {
     let canvas = document.getElementById("canvas");
-
-    console.log("handleROI()");
     if (dots >= 4) {
       alert("All ROI points drawn!");
     } else {
@@ -149,63 +173,109 @@ export default function Canvas() {
     let y = e.clientY - rect.top;
     console.log(x, y);
     if (!inside([x, y], finalROI)) {
-      alert("draw inside ROI");
+      // alert("draw inside ROI");
       return;
     }
     if (LOIClicks != 1) {
       LOIClicks++;
-      lastClick = [{ x, y }];
+      lastClick = [{ x1: x, y1: y }];
       drawCoordinates(x, y);
-      console.log();
     } else {
       ctx.beginPath();
-      ctx.moveTo(lastClick[0].x, lastClick[0].y);
+      ctx.moveTo(lastClick[0].x1, lastClick[0].y1);
       ctx.lineTo(x, y, 6);
       ctx.strokeStyle = "#ff2626";
       ctx.stroke();
       LOIClicks = 0;
-      lastClick = [...lastClick, { x1: x, y1: y }];
-      console.log(inside([x, y], finalROI));
+      lastClick = [...lastClick, { x2: x, y2: y }];
+      setLOICord((oldArray) => [
+        ...oldArray,
+        {
+          x1: lastClick[0].x1,
+          y1: lastClick[0].y1,
+          x2: lastClick[1].x2,
+          y2: lastClick[1].y2,
+        },
+      ]);
     }
-    // console.log(lastClick);
+    console.log(lastClick);
+  };
+
+  const drawROILOI = () => {
+    let _roi = staticData.roi;
+    let _loi = staticData.loi;
+    dots = 4;
+    _roi.map((item) => {
+      finalROI.push([item.x, item.y]);
+    });
+    redrawCanvas(_roi, _loi);
   };
 
   useEffect(() => {
     if (dots === 4) {
+      setType("LOI");
       drawROI([ROICord[2].x, ROICord[2].y], [ROICord[3].x, ROICord[3].y]);
       drawROI([ROICord[3].x, ROICord[3].y], [ROICord[0].x, ROICord[0].y]);
     }
   }, [ROICord]);
 
+  useEffect(() => {
+    drawROILOI();
+  }, []);
+
   return (
-    <div
-      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
-    >
-      {console.log(ROICord, finalROI)}
-      <p>canvas</p>
-      <p>Type: {Type}</p>
-      <canvas
-        id="canvas"
-        width="640px"
-        height="480px"
-        style={{ margin: "1vw", background: "gray", alignSelf: "center" }}
-        onClick={(e) => {
-          if (Type === "ROI") handleROI(e);
-          else handleLOI(e);
-        }}
-      />
-      <button onClick={() => setType("ROI")}>ROI</button>
-      <button onClick={() => setType("LOI")}>LOI</button>
-      <button
-        onClick={() => {
-          let ctx = document.getElementById("canvas").getContext("2d");
-          dots = 0;
-          setROICord([]);
-          ctx.clearRect(0, 0, 640, 480);
-        }}
-      >
-        Clear
-      </button>
+    <div style={{ display: "flex" }}>
+      {console.log(ROICord, LOICord, finalROI)}
+      <div>
+        <p>canvas</p>
+        <p>Type: {Type}</p>
+        <canvas
+          id="canvas"
+          width="640px"
+          height="480px"
+          style={{ margin: "1vw", background: "gray", alignSelf: "center" }}
+          onClick={(e) => {
+            if (Type === "ROI") handleROI(e);
+            else handleLOI(e);
+          }}
+        />
+      </div>
+      <div>
+        <div style={{ display: "flex" }}>
+          <button disabled={dots === 4} onClick={() => setType("ROI")}>
+            ROI
+          </button>
+          <button onClick={() => setType("LOI")}>LOI</button>
+
+          <button
+            onClick={() => {
+              let ctx = document.getElementById("canvas").getContext("2d");
+              dots = 0;
+              setROICord([]);
+              setType("ROI");
+              ctx.clearRect(0, 0, 640, 480);
+            }}
+          >
+            Clear
+          </button>
+          <button onClick={() => setType("LOI")}>Add new LOI</button>
+        </div>
+        {LOICord.map((item, idx) => (
+          <div>
+            <p>LOI-{idx + 1}</p>
+            <button
+              onClick={() => {
+                let _data = [...LOICord];
+                _data.splice(idx, 1);
+                setLOICord([..._data]);
+                redrawCanvas(ROICord, _data);
+              }}
+            >
+              Remove
+            </button>
+          </div>
+        ))}
+      </div>
       {/* <input
         type={"text"}
         onChange={(e) => setRows(e.target.value)}
@@ -226,3 +296,18 @@ export default function Canvas() {
     </div>
   );
 }
+
+const staticData = {
+  roi: [
+    { x: 139.75, y: 74.75 },
+    { x: 532.75, y: 51.75 },
+    { x: 550.75, y: 274.75 },
+    { x: 168.75, y: 312.75 },
+  ],
+  loi: [
+    { x1: 157.75, y1: 86.75, x2: 531.75, y2: 264.75 },
+    { x1: 526.75, y1: 55.75, x2: 176.75, y2: 303.75 },
+    { x1: 340.75, y1: 66.75, x2: 361.75, y2: 287.75 },
+    { x1: 164.75, y1: 205.75, x2: 533.75, y2: 151.75 },
+  ],
+};
